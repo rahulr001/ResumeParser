62b9ea56b2d6e457e32077e197e0f11d930a4607
Rahul R <109589036+rahulr001@users.noreply.github.com>
2023-07-07 16:05:50 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
 import NavBar from "./components/NabBar";
 import Home from "./components/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DataList from "./components/DataList";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>

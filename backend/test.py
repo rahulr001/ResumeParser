8ccadede31788321a409224d33f03e6dab1f9a0f
Rahul R <109589036+rahulr001@users.noreply.github.com>
2023-07-07 15:25:55 +0530

    Delete test.py

--- backend/test.py
+++
@@ -1,6 +0,0 @@
-from pyresparser import ResumeParser
-data = ResumeParser('Resume.pdf').get_extracted_data()
-
-# from resume_parser import resumeparse
-# data = resumeparse.read_file('Resume.pdf')
-print(data)

7aa44026e904b0ee052fa1b3e9e5c9aec9ad5e03
Rahul R <109589036+rahulr001@users.noreply.github.com>
2023-07-10 17:22:13 +0530

    minor improvements
    minor improvements

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,7 @@ from schemas import user_type
 from fastapi.middleware.cors import CORSMiddleware
 from bson.objectid import ObjectId
 
+
 app = FastAPI()
 
 app.add_middleware(
--- backend/parser.py
+++ backend/parser.py
@@ -1,20 +1,18 @@
 import re
-# import docx2txt
 from pdfminer.high_level import extract_text
-# from docx import Document
+from tika import parser
 from pyresparser import ResumeParser
+import threading
+from lazy_import import lazy_module
+from resume_parser import resumeparse
+resume_parser = lazy_module('resume_parser.resumeparse')
+# t1 = threading.Thread(target=resumeparse)
+# t1.start()
+print(dir(resume_parser.resumeparse))
+# t1.join()
 
-
-# def extract_text_from_pdf(file_path: str) -> str:
-#     if file_path.endswith(".docx"):
-#         doc = Document(file_path)
-#         text = ""
-#         for paragraph in doc.paragraphs:
-#             text += paragraph.text + "\n"
-#         return text
-#     elif file_path.endswith(".doc"):
-#         pass
-#     return extract_text(file_path)
+# def extract_text(file_path: str) -> str:
+#     return parser.from_file(file_path,service='text')["content"]
 
 
 # def extract_contact_number_from_resume(text: str) -> int:
@@ -40,27 +38,27 @@ from pyresparser import ResumeParser
 
 def extract_contact_number_from_resume(file_path: str) -> int:
     if file_path.endswith('.doc'):
-        from resume_parser import resumeparse
+        # from resume_parser import resumeparse
         return resumeparse.read_file(file_path)['phone']
     return ResumeParser(file_path).get_extracted_data()['mobile_number']
 
 
 def extract_email_from_resume(file_path: str) -> str:
     if file_path.endswith('.doc'):
-        from resume_parser import resumeparse
+        # from resume_parser import resumeparse
         return resumeparse.read_file(file_path)['email']
     return ResumeParser(file_path).get_extracted_data()['email']
 
 
 def extract_name_from_resume(file_path: str) -> str:
     if file_path.endswith('.doc'):
-        from resume_parser import resumeparse
+        # from resume_parser import resumeparse
         return resumeparse.read_file(file_path)['name']
     return ResumeParser(file_path).get_extracted_data()['name']
 
 
 if __name__ == '__main__':  
-    # text = extract_text_from_pdf(f'Resume.docx')
-    print(extract_contact_number_from_resume(f'Resume.pdf'))
-    print(extract_email_from_resume(f'Resume.pdf'))
-    print(extract_name_from_resume(f'Resume.pdf'))
+    # text = extract_text(f'Resume.docx')
+    print(extract_contact_number_from_resume(f'Resume.doc'))
+    print(extract_email_from_resume(f'Resume.doc'))
+    print(extract_name_from_resume(f'Resume.doc'))
